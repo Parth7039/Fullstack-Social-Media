e2dfc1a1d031d18cb6cc358c838502cf98df557a
Parth7039 <p.k.bhamare07@gmail.com>
2025-06-07 12:15:09 +0530

    Drawer done and profile page created
    Signed-off-by: Parth7039 <p.k.bhamare07@gmail.com>

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_media_fullstack/features/auth/presentation/cubits/auth_cubit.dart';
+
+import '../../../auth/domain/entities/app_user.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -8,6 +12,11 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+
+  late final authCubit = context.read<AuthCubit>();
+
+  late AppUser? currentuser = authCubit.currentUser;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
